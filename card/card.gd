79b2ff624cb8e9f3b0b98e62d6abeda01a59528a
CattacoCattaco <cattacoiscool@gmail.com>
2025-11-16 07:11:51 -0800

    Effect Text is working
    The card now auto-generates text for its effects based on what modifiers it has.

--- card/card.gd
+++ card/card.gd
@@ -7,10 +7,16 @@ extends Control
 @export var range_label: Label
 @export var effect_size_label: Label
 
-@export var modifiers: Array[Modifier] = [Modifier.Move.new(), Modifier.Attack.new(),
-		Modifier.Jump.new(), Modifier.Poison.new(), Modifier.Heal.new()]
 @export var effect_range: int
 @export var effect_size: int
+
+var modifiers: Array[Modifier] = [
+	Modifier.Heal.new(),
+	Modifier.Attack.new(),
+	Modifier.Poison.new(),
+	Modifier.Jump.new(),
+	Modifier.Split2.new(),
+]
 
 
 func _ready() -> void:
@@ -30,3 +36,79 @@ func load_data() -> void:
 	
 	range_label.text = str(effect_range)
 	effect_size_label.text = str(effect_size)
+	
+	var effects: Array[Effect] = get_effects()
+	
+	effect_label.text = get_effect_text(effects)
+
+
+func get_effects() -> Array[Effect]:
+	var effects: Array[Effect] = []
+	
+	var base_actions: Array[Modifier] = []
+	var local_mods: Array[Modifier.ModifierModifier] = []
+	var global_mods: Array[Modifier.ModifierModifier] = []
+	
+	for modifier in modifiers:
+		match modifier.get_mod_type():
+			Modifier.Type.BASE_ACTION:
+				base_actions.append(modifier)
+			Modifier.Type.LOCAL_MOD:
+				local_mods.append(modifier)
+			Modifier.Type.GLOBAL_MOD:
+				global_mods.append(modifier)
+	
+	for base_action in base_actions:
+		var applicable_local_mods: Array[Modifier.ModifierModifier] = []
+		for local_mod in local_mods:
+			if local_mod.applies_to(base_action):
+				applicable_local_mods.append(local_mod)
+		
+		effects.append(Effect.create(base_action, applicable_local_mods, global_mods))
+	
+	return effects
+
+
+func get_effect_text(effects: Array[Effect]) -> String:
+	if len(effects) == 0:
+		return "(Blank)"
+	
+	var effect_text: String = ""
+	
+	for effect in effects:
+		if effect_text != "":
+			effect_text += " "
+		
+		effect_text += effect.get_text(effect_range, effect_size)
+	
+	for global_mod in effects[0].global_modifiers:
+		effect_text += global_mod._get_text(effect_range, effect_size)
+	
+	return effect_text
+
+
+class Effect extends RefCounted:
+	var base_action: Modifier
+	var local_modifiers: Array[Modifier.ModifierModifier]
+	var global_modifiers: Array[Modifier.ModifierModifier]
+	
+	static func create(p_base_action: Modifier, p_local_modifiers: Array[Modifier.ModifierModifier], 
+			p_global_modifiers: Array[Modifier.ModifierModifier]) -> Effect:
+		
+		var effect: Effect = Effect.new()
+		
+		effect.base_action = p_base_action
+		effect.local_modifiers = p_local_modifiers
+		effect.global_modifiers = p_global_modifiers
+		
+		return effect
+	
+	
+	func get_text(effect_range: int, effect_size: int) -> String:
+		var sentence: String = base_action._get_text(effect_range, effect_size)
+		
+		for local_mod in local_modifiers:
+			sentence += local_mod._get_text(effect_range, effect_size)
+		
+		sentence += "."
+		return sentence
--- card/modifier/modifier.gd
+++ card/modifier/modifier.gd
@@ -1,10 +1,12 @@
 @abstract
 class_name Modifier
-extends Resource
+extends RefCounted
 
-@abstract func _get_name() -> String
-@abstract func _get_image() -> Texture2D
-@abstract func _get_sort_order() -> int
+enum Type {
+	BASE_ACTION,
+	LOCAL_MOD,
+	GLOBAL_MOD,
+}
 
 
 static func sort(modifiers: Array[Modifier]) -> void:
@@ -15,9 +17,35 @@ static func a_before_b(a: Modifier, b: Modifier) -> bool:
 	return a._get_sort_order() < b._get_sort_order()
 
 
+## Returns the name of this modifier in title case
+@abstract func _get_name() -> String
+## Returns the sprite associated with this modifier
+@abstract func _get_image() -> Texture2D
+## Lower numbers go to the left of higher numbers
+## 0-9: Base actions
+## 10-19: Local modifiers
+## 20-29: Global modifiers
+@abstract func _get_sort_order() -> int
+## For base actions, the description of the effect with the first word capitalized
+## For Local modifiers, the modification with no capitalization prefixed with a space
+## For Global modifiers, a complete sentence of the change with a period prefixed with a space
+@abstract func _get_text(effect_range: int, effect_size: int) -> String
+
+
+func get_mod_type() -> Type:
+	var sort_order: int = _get_sort_order()
+	
+	if sort_order < 10:
+		return Type.BASE_ACTION
+	elif sort_order < 20:
+		return Type.LOCAL_MOD
+	else:
+		return Type.GLOBAL_MOD
+
+
 class Move extends Modifier:
 	func _get_name() -> String:
-		return "move"
+		return "Move"
 	
 	
 	func _get_image() -> Texture2D:
@@ -26,11 +54,18 @@ class Move extends Modifier:
 	
 	func _get_sort_order() -> int:
 		return 0
+	
+	
+	func _get_text(effect_range: int, _effect_size: int) -> String:
+		if effect_range == 1:
+			return "Move 1 space"
+		
+		return "Move up to %d spaces" % effect_range
 
 
 class Attack extends Modifier:
 	func _get_name() -> String:
-		return "attack"
+		return "Attack"
 	
 	
 	func _get_image() -> Texture2D:
@@ -39,11 +74,15 @@ class Attack extends Modifier:
 	
 	func _get_sort_order() -> int:
 		return 1
+	
+	
+	func _get_text(effect_range: int, effect_size: int) -> String:
+		return "Deal %d damage to a target in range %d" % [effect_size, effect_range]
 
 
 class Heal extends Modifier:
 	func _get_name() -> String:
-		return "heal"
+		return "Heal"
 	
 	
 	func _get_image() -> Texture2D:
@@ -52,11 +91,15 @@ class Heal extends Modifier:
 	
 	func _get_sort_order() -> int:
 		return 2
+	
+	
+	func _get_text(effect_range: int, effect_size: int) -> String:
+		return "Heal %d damage from a target in range %d" % [effect_size, effect_range]
 
 
 class Poison extends Modifier:
 	func _get_name() -> String:
-		return "poison"
+		return "Poison"
 	
 	
 	func _get_image() -> Texture2D:
@@ -65,29 +108,20 @@ class Poison extends Modifier:
 	
 	func _get_sort_order() -> int:
 		return 3
+	
+	
+	func _get_text(effect_range: int, effect_size: int) -> String:
+		return "Apply %d poison to a target in range %d" % [effect_size, effect_range]
 
 
 @abstract
 class ModifierModifier extends Modifier:
-	pass
-
-
-class Jump extends ModifierModifier:
-	func _get_name() -> String:
-		return "jump"
-	
-	
-	func _get_image() -> Texture2D:
-		return preload("res://card/modifier/jump.png")
-	
-	
-	func _get_sort_order() -> int:
-		return 10
+	@abstract func applies_to(modifier: Modifier) -> bool
 
 
 class Split2 extends ModifierModifier:
 	func _get_name() -> String:
-		return "split2"
+		return "Split"
 	
 	
 	func _get_image() -> Texture2D:
@@ -95,12 +129,20 @@ class Split2 extends ModifierModifier:
 	
 	
 	func _get_sort_order() -> int:
-		return 11
+		return 10
+	
+	
+	func applies_to(modifier: Modifier) -> bool:
+		return modifier is not Move
+	
+	
+	func _get_text(_effect_range: int, _effect_size: int) -> String:
+		return " twice at the same time"
 
 
 class Split3 extends ModifierModifier:
 	func _get_name() -> String:
-		return "split3"
+		return "Split 3 Way"
 	
 	
 	func _get_image() -> Texture2D:
@@ -108,4 +150,37 @@ class Split3 extends ModifierModifier:
 	
 	
 	func _get_sort_order() -> int:
-		return 12
+		return 11
+	
+	
+	func applies_to(modifier: Modifier) -> bool:
+		return modifier is not Move
+	
+	
+	func _get_text(_effect_range: int, _effect_size: int) -> String:
+		return " thrice at the same time"
+
+
+class Jump extends ModifierModifier:
+	func _get_name() -> String:
+		return "Jump"
+	
+	
+	func _get_image() -> Texture2D:
+		return preload("res://card/modifier/jump.png")
+	
+	
+	func _get_sort_order() -> int:
+		return 20
+	
+	
+	func applies_to(_modifier: Modifier) -> bool:
+		return true
+	
+	
+	func _get_text(effect_range: int, _effect_size: int) -> String:
+		# Does nothing if range is only one space
+		if effect_range == 1:
+			return ""
+		
+		return " Range jumps over blocked spaces."
