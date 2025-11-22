a666bae68bc21b60c4a2174cdee11d712557a718
CattacoCattaco <cattacoiscool@gmail.com>
2025-11-22 15:50:57 -0800

    Create TileGrid
    Created TileGrid, which creates and holds a rectangle of Tiles as children. Also created DraggableCamera to be able to view various regions on the TileGrid

---
+++ camera/draggable_camera.gd
@@ -0,0 +1,43 @@
+class_name DraggableCamera
+extends Camera2D
+
+@export var tile_grid: TileGrid
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			position -= event.relative
+			
+			if tile_grid:
+				fit_pos_to_bounds(tile_grid.get_camera_bounds())
+	elif event is InputEventKey:
+		if event.is_action_pressed("zoom"):
+			print(zoom)
+			if zoom == Vector2(1, 1):
+				zoom = Vector2(2, 2)
+			else:
+				zoom = Vector2(1, 1)
+
+
+func fit_pos_to_bounds(bounds: Rect2) -> void:
+	var half_view_size: Vector2 = get_viewport_rect().size / zoom / 2
+	var view_top_left: Vector2 = position - half_view_size
+	var view_bottom_right: Vector2 = position + half_view_size
+	
+	var bounds_top_left: Vector2 = bounds.position
+	var bounds_bottom_right: Vector2 = bounds.position + bounds.size
+	
+	if view_top_left.x < bounds_top_left.x and view_bottom_right.x > bounds_bottom_right.x:
+		position.x = 0
+	elif view_top_left.x < bounds_top_left.x:
+		position.x += (bounds_top_left - view_top_left).x
+	elif view_bottom_right.x > bounds_bottom_right.x:
+		position.x += (bounds_bottom_right - view_bottom_right).x
+	
+	if view_top_left.y < bounds_top_left.y and view_bottom_right.y > bounds_bottom_right.y:
+		position.y = 0
+	elif view_top_left.y < bounds_top_left.y:
+		position.y += (bounds_top_left - view_top_left).y
+	elif view_bottom_right.y > bounds_bottom_right.y:
+		position.y += (bounds_bottom_right - view_bottom_right).y
---
+++ tile_grid/tile_grid.gd
@@ -0,0 +1,39 @@
+class_name TileGrid
+extends Node2D
+
+@export var tile_scene: PackedScene
+@export var camera_padding := Vector2i(128, 128)
+
+@export var size := Vector2i(15, 10)
+
+var tiles: Array[Array] = []
+
+
+func _ready() -> void:
+	var pixel_size: Vector2 = size * 32
+	var offset := -Vector2(pixel_size) / 2
+	
+	for x in range(size.x):
+		var column: Array[Tile] = []
+		
+		for y in range(size.y):
+			var tile: Tile = tile_scene.instantiate()
+			
+			add_child(tile)
+			column.append(tile)
+			
+			tile.pos = Vector2i(x, y)
+			tile.position = Vector2(32 * x, 32 * y) + offset
+		
+		tiles.append(column)
+
+
+func get_camera_bounds() -> Rect2:
+	var bottom_right: Vector2 = size * 16
+	var top_left: Vector2 = -bottom_right
+	
+	var bounds := Rect2()
+	bounds.position = top_left - Vector2(camera_padding)
+	bounds.size = Vector2(size * 32 + camera_padding * 2)
+	
+	return bounds
