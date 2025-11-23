eab0f93a759187e189557b9fdbec3d8a9b2c6675
CattacoCattaco <cattacoiscool@gmail.com>
2025-11-23 15:21:49 -0800

    Add Designated Movement Region
    Added a designated movement region, which is guaranteed to be connected so the player and enemies can safely be put there. There is also a density setting which determines how big the region is

--- tile_grid/level_builder/level_builder.gd
+++ tile_grid/level_builder/level_builder.gd
@@ -1,22 +1,58 @@
 class_name LevelBuilder
 extends Node2D
 
+enum ObjectDensity {
+	SPARSE,
+	MILD,
+	FEATUREFUL,
+	DENSE,
+}
+
 @export var tile_grid: TileGrid
 
+@export var density: ObjectDensity
+@export var movement_region_object_data: TileObjectData
 @export var clump_obstacles: Array[TileObjectData]
 @export var clump_obstacle_weights: Array[int]
 @export var single_obstacles: Array[TileObjectData]
 @export var single_obstacle_weights: Array[int]
 
+var untouched_cells: Array[Vector2i]
+
 
 func place_objects() -> void:
 	var tile_count: int = tile_grid.size.x * tile_grid.size.y
 	
-	var untouched_cells: Array[Vector2i] = []
+	untouched_cells = []
 	
 	for x in range(tile_grid.size.x):
 		for y in range(tile_grid.size.y):
 			untouched_cells.append(Vector2i(x, y))
+	
+	var movement_region_origin := Vector2i(randi_range(2, tile_grid.size.x - 2), 
+			randi_range(2, tile_grid.size.y - 2))
+	var designated_movement_region: Array[Vector2i] = [movement_region_origin]
+	var movement_region_size: int
+	
+	match density:
+		ObjectDensity.SPARSE:
+			movement_region_size = ceili(randf_range(0.6 * tile_count, 0.8 * tile_count))
+		ObjectDensity.MILD:
+			movement_region_size = ceili(randf_range(0.45 * tile_count, 0.6 * tile_count))
+		ObjectDensity.FEATUREFUL:
+			movement_region_size = ceili(randf_range(0.25 * tile_count, 0.45 * tile_count))
+		ObjectDensity.DENSE:
+			movement_region_size = ceili(randf_range(0.1 * tile_count, 0.25 * tile_count))
+	
+	untouched_cells.erase(movement_region_origin)
+	
+	while len(designated_movement_region) < movement_region_size:
+		walk(movement_region_origin, designated_movement_region, movement_region_size, 
+				[Vector2i(0, 1), Vector2i(1, 0), Vector2i(0, -1), Vector2i(-1, 0)].pick_random())
+	
+	for pos in designated_movement_region:
+		var tile: Tile = tile_grid.tiles[pos.x][pos.y]
+		tile.add_object(movement_region_object_data)
 	
 	var clump_count: int = floori(randf_range(tile_count / 80.0, tile_count / 25.0))
 	for i in range(clump_count):
@@ -53,6 +89,53 @@ func place_objects() -> void:
 		
 		var tile: Tile = tile_grid.tiles[pos.x][pos.y]
 		tile.add_object(pick_random_weighted(single_obstacles, single_obstacle_weights))
+
+
+func walk(current_pos: Vector2i, region: Array[Vector2i], goal_size: int, from: Vector2i,
+		branch_length: int = 0) -> void:
+	if len(region) >= goal_size:
+		return
+	
+	var valid_neighbors: Array[Vector2i] = []
+	var semivalid_neighbors: Array[Vector2i] = []
+	
+	for dir in [Vector2i(0, 1), Vector2i(1, 0), Vector2i(0, -1), Vector2i(-1, 0)]:
+		if current_pos + dir in untouched_cells:
+			valid_neighbors.append(current_pos + dir)
+		elif len(valid_neighbors) == 0 and current_pos + dir in region:
+			semivalid_neighbors.append(current_pos + dir)
+	
+	if randf() < 0.025 * branch_length:
+		return
+	
+	# Add 1 to 3 tiles, with a bias towards making 1
+	for i in range([1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3].pick_random()):
+		# 60% chance to keep going in current direction if possible
+		if current_pos + from in valid_neighbors and randf() < 0.6:
+			var next_pos: Vector2i = current_pos + from
+			
+			region.append(next_pos)
+			untouched_cells.erase(next_pos)
+			walk(next_pos, region, goal_size, from)
+			
+			valid_neighbors.erase(next_pos)
+		# Try to add new spaces before walking to existing spaces
+		elif len(valid_neighbors) > 0:
+			var next_pos: Vector2i = valid_neighbors.pick_random()
+			
+			region.append(next_pos)
+			untouched_cells.erase(next_pos)
+			walk(next_pos, region, goal_size, next_pos - current_pos)
+			
+			valid_neighbors.erase(next_pos)
+		# Try to walk to existing spaces before giving up
+		elif len(semivalid_neighbors) > 0:
+			var next_pos: Vector2i = semivalid_neighbors.pick_random()
+			
+			walk(next_pos, region, goal_size, next_pos - current_pos)
+			
+			# We shouldn't make too many branches here
+			continue
 
 
 func pick_random_weighted(data_list: Array[TileObjectData], weights: Array[int]) -> TileObjectData:
