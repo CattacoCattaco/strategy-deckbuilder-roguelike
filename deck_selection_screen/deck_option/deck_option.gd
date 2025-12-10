ba8a4594e40e28dfbea39fb856b78674a914a621
CattacoCattaco <cattacoiscool@gmail.com>
2025-12-10 07:19:34 -0800

    Add Deck Selection Screen
    Added deck selection screen, which lets you choose what deck you start with before the game starts

---
+++ deck_selection_screen/deck_option/deck_option.gd
@@ -0,0 +1,5 @@
+class_name DeckOption
+extends Control
+
+@export var name_label: Label
+@export var select_button: Button
---
+++ deck_selection_screen/deck_selection_screen.gd
@@ -0,0 +1,64 @@
+class_name DeckSelectionScreen
+extends Control
+
+static var DECKS: Dictionary[String, Array] = {
+	"Basic Deck": [
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Attack.new()], 1, 1),
+		CardData.new([Modifier.Attack.new()], 1, 1),
+		CardData.new([Modifier.Attack.new()], 1, 1),
+		CardData.new([Modifier.Attack.new()], 1, 1),
+		CardData.new([Modifier.Attack.new()], 1, 1),
+		CardData.new([Modifier.Heal.new()], 1, 1),
+		CardData.new([Modifier.Heal.new()], 1, 1),
+	],
+	"Poison Deck": [
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Move.new()], 1, 1),
+		CardData.new([Modifier.Poison.new()], 1, 1),
+		CardData.new([Modifier.Poison.new()], 1, 1),
+		CardData.new([Modifier.Poison.new()], 1, 1),
+		CardData.new([Modifier.Poison.new()], 1, 1),
+		CardData.new([Modifier.Poison.new()], 1, 1),
+		CardData.new([Modifier.Poison.new()], 1, 1),
+	],
+}
+
+@export var world_map_scene: PackedScene
+@export var deck_option_scene: PackedScene
+
+@export var deck_options_container: HBoxContainer
+
+
+func _ready() -> void:
+	for deck_name in DECKS:
+		var contents: Array[CardData] = []
+		for card in DECKS[deck_name]:
+			contents.append(card)
+		
+		_add_deck_option(contents, deck_name)
+
+
+func _add_deck_option(deck_contents: Array[CardData], deck_name: String) -> void:
+	var deck_option: DeckOption = deck_option_scene.instantiate()
+	
+	deck_options_container.add_child(deck_option)
+	
+	deck_option.name_label.text = deck_name
+	deck_option.select_button.pressed.connect(_choose_deck.bind(deck_contents))
+
+
+func _choose_deck(deck: Array[CardData]) -> void:
+	var world_map: WorldMap = world_map_scene.instantiate()
+	world_map.player_deck = deck
+	
+	get_tree().root.add_child(world_map)
+	queue_free()
