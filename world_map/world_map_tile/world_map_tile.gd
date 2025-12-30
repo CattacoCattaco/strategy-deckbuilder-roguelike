6af03c703aadd6c834cbf2d642421da5a1bafd99
CattacoCattaco <cattacoiscool@gmail.com>
2025-12-30 10:07:20 -0800

    Reorder EventType
    EventType.NONE is now 0 rather than being the last item in case EventType gains more elements

--- world_map/world_map_tile/world_map_tile.gd
+++ world_map/world_map_tile/world_map_tile.gd
@@ -2,6 +2,7 @@ class_name WorldMapTile
 extends Node2D
 
 enum EventType {
+	NONE,
 	ENTRANCE,
 	EXIT,
 	ENCOUNTER,
@@ -13,7 +14,6 @@ enum EventType {
 	DESTROY_CARD,
 	DRAFT,
 	STAT_SWAP,
-	NONE,
 }
 
 const POSITIVE_EVENT_UNLOCKS: Dictionary[EventType, int] = {
