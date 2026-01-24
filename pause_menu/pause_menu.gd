3958e61bd558283aac7bf48acb4c7b287cc0583c
CattacoCattaco <cattacoiscool@gmail.com>
2026-01-24 06:28:10 -0800

    Audio Settings Effect Noise Volume
    Now the audio settings actually change the volume of the sounds that they are supposed to affect. Temporarily, the sliders play the sound when you change the volume to give a preview. The signal is called to frequently for this to be practical normally

--- pause_menu/pause_menu.gd
+++ pause_menu/pause_menu.gd
@@ -9,6 +9,7 @@ extends Control
 
 @export var audio_settings_menu: GridContainer
 @export var audio_settings_sliders: Array[Slider]
+@export var action_noise_player: ActionNoisePlayer
 
 
 func _ready() -> void:
@@ -43,4 +44,27 @@ func close_audio_settings() -> void:
 
 func change_audio_setting(new_value: float, setting: Settings.AudioSetting) -> void:
 	Settings.audio_settings[setting] = roundi(new_value)
-	print(Settings.audio_settings)
+	
+	match setting:
+		Settings.AudioSetting.LOSE_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.LOSE)
+		Settings.AudioSetting.WIN_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.WIN)
+		Settings.AudioSetting.ACTION_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.DAMAGE)
+		Settings.AudioSetting.ATTACK_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.DAMAGE)
+		Settings.AudioSetting.DESTROY_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.DESTROY)
+		Settings.AudioSetting.HEAL_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.HEAL)
+		Settings.AudioSetting.MOVE_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.MOVE)
+		Settings.AudioSetting.POISON_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.POISON)
+		Settings.AudioSetting.PUSH_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.PUSH)
+		Settings.AudioSetting.SHIELD_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.DEFEND)
+		Settings.AudioSetting.SWAP_VOLUME:
+			action_noise_player.play_sound(ActionNoisePlayer.Sound.SWAP)
--- sounds/action_noise_player.gd
+++ sounds/action_noise_player.gd
@@ -19,31 +19,103 @@ func play_sound(sound: Sound) -> void:
 	match sound:
 		Sound.DAMAGE:
 			stream = preload("res://sounds/action_noises/damage.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.ACTION_VOLUME] / 100.0
+			mult *= Settings.audio_settings[Settings.AudioSetting.ATTACK_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.DEFEND:
 			stream = preload("res://sounds/action_noises/defend.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.ACTION_VOLUME] / 100.0
+			mult *= Settings.audio_settings[Settings.AudioSetting.SHIELD_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.DESTROY:
 			stream = preload("res://sounds/action_noises/destroy.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.ACTION_VOLUME] / 100.0
+			mult *= Settings.audio_settings[Settings.AudioSetting.DESTROY_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.HEAL:
 			stream = preload("res://sounds/action_noises/heal.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.ACTION_VOLUME] / 100.0
+			mult *= Settings.audio_settings[Settings.AudioSetting.HEAL_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.LOSE:
 			stream = preload("res://sounds/action_noises/lose.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.LOSE_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.MOVE:
 			stream = preload("res://sounds/action_noises/move.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.ACTION_VOLUME] / 100.0
+			mult *= Settings.audio_settings[Settings.AudioSetting.MOVE_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.POISON:
 			stream = preload("res://sounds/action_noises/poison.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.ACTION_VOLUME] / 100.0
+			mult *= Settings.audio_settings[Settings.AudioSetting.POISON_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.PUSH:
 			stream = preload("res://sounds/action_noises/push.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.ACTION_VOLUME] / 100.0
+			mult *= Settings.audio_settings[Settings.AudioSetting.PUSH_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.SWAP:
 			stream = preload("res://sounds/action_noises/swap.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.ACTION_VOLUME] / 100.0
+			mult *= Settings.audio_settings[Settings.AudioSetting.SWAP_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
 		Sound.WIN:
 			stream = preload("res://sounds/action_noises/win.wav")
+			
+			# Volume multiplier
+			var mult: float = 1
+			mult *= Settings.audio_settings[Settings.AudioSetting.WIN_VOLUME] / 100.0
+			volume_db = mult_to_db(mult)
+			
 			play()
+
+
+func mult_to_db(mult: float) -> float:
+	return log(mult) / log(10) * 10
